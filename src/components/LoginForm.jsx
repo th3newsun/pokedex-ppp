a51b3ee470ac7c49992ff5e45536448c38cfab2f
th3newsun <111325050+th3newsun@users.noreply.github.com>
2024-11-17 22:58:24 -0300

    Funcionalidade de cadastro e login prontas/erro de direcionamento corrigido (eu acho)

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,26 +1,22 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { auth } from '../firebaseConfig.js'; // Importe a instância de auth do seu firebaseConfig
-import { signInWithEmailAndPassword } from 'firebase/auth'; // Importe a função de login do Firebase
+import { auth } from '../firebaseConfig.js'; 
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 
 const LoginForm = () => {
-  // Estados para os inputs de email e senha, e para mensagens de erro
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const navigate = useNavigate(); // Navegação após login bem-sucedido
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-    e.preventDefault(); // Impede o comportamento padrão de envio do formulário
+    e.preventDefault();
 
     try {
-      // Tenta fazer login com email e senha
       await signInWithEmailAndPassword(auth, email, password);
-      // Se o login for bem-sucedido, redireciona para a página inicial
-      navigate('/PokemonList');
+      navigate('/Home');
     } catch (error) {
-      // Em caso de erro, exibe a mensagem
       setError('Email ou senha inválidos');
     }
   };
@@ -30,26 +26,22 @@ const LoginForm = () => {
       <form className="bg-[#dce6e7] p-5 rounded-2xl h-full flex flex-col items-center" onSubmit={handleSubmit}>
         <h1 className='font-bold font-outfit text-[1.82vw] mt-3'>LOGIN</h1>
         <hr className="border-t-[2px] border-black w-full mt-8 mb-8"></hr>
-        
-        {/* Exibe mensagem de erro, se houver */}
         {error && <p className="text-red-500 mb-4">{error}</p>}
-
         <div>
           <input
             type="email"
             name="email"
             value={email}
-            onChange={(e) => setEmail(e.target.value)} // Atualiza o estado do email
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
             required
             className="shadow appearance-none font-outfit border rounded w-full py-5 px-4 text-gray-700 mb-4 leading-tight focus:outline-none focus:shadow-outline"
           />
-          
           <input
             type="password"
             name="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)} // Atualiza o estado da senha
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="Senha"
             required
             className="shadow appearance-none font-outfit border rounded w-full py-5 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -58,7 +50,7 @@ const LoginForm = () => {
 
         <hr className="border-t-[2px] border-black w-full mt-8 mb-8"></hr>
 
-        {/* Botão de envio do formulário */}
+        {}
         <button
           type="submit"
           className="bg-red-600 hover:bg-red-500 text-white text-[1vw] font-bold font-outfit py-3 px-14 mb-3 rounded focus:outline-none focus:shadow-outline"
--- src/components/SingUpPageForm.jsx
+++ src/components/SingUpPageForm.jsx
@@ -1,44 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../firebaseConfig';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 const SingUpPageForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      navigate('/');
+    } catch (err) {
+      console.error(err.message);
+      setError('Erro ao criar conta. Tente novamente.');
+    }
+  };
+
   return (
     <section>
-      
-      <div className='bg-white col-start-2 w-full p-7'>
-        <h1 className='font-bold font-outfit text-[25px] cursor-default'>Cadastre-se</h1>
+      <div className="bg-white col-start-2 w-full p-7">
+        <h1 className="font-bold font-outfit text-[25px] cursor-default">Cadastre-se</h1>
         <hr className="border-t-[2px] border-black mt-4 mb-4"></hr>
-        <h2 className='font-outfit text-[20px] mb-2 mt-4 block cursor-default'>Nome</h2>
-        <div className="flex gap-3 justify-center mt-2">
-          <input type="text" name="firstName" placeholder="Nome" required className="appearance-none font-outfit rounded py-5 px-4 text-gray-700 leading-tight focus:outline-none h-[54px] w-[50%] border border-gray-400" />
 
-          <input type="text" name="lastName" placeholder="Sobrenome" required className="appearance-none font-outfit rounded py-5 px-4 text-gray-700 focus:outline-none h-[54px] w-[50%] border border-gray-400" />
-        </div>
-        <h2 className='font-outfit text-[20px] mb-2 mt-4 block cursor-default'>Email</h2>
-        <div>
-          <input type="email" name="email" placeholder="Email" required className="appearance-none font-outfit border border-gray-400 rounded w-full h-[54px] py-5 px-4 text-gray-700 mb-2 focus:outline-none" />
-        </div>
+        {error && <p className="text-red-500 mb-4">{error}</p>}
 
-        <h2 className='font-outfit cursor-default text-[20px] mb-2 mt-4 block' >Gênero</h2>
-        <fieldset className='flex justify-center gap-3'>
-          <div className="border border-gray-400 rounded h-[35px] w-[50%] flex items-center">
-            <input type="radio" id="male" name="gender" value="male" className="ml-3" />
-              <label htmlFor="male" className="font-outfit p-3 text-[17px] text-gray-500">Masculino</label>
+        <form onSubmit={handleSubmit}>
+          <h2 className="font-outfit text-[20px] mb-2 mt-4 block cursor-default">Email</h2>
+          <div>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              required
+              className="appearance-none font-outfit border border-gray-400 rounded w-full h-[54px] py-5 px-4 text-gray-700 mb-2 focus:outline-none"
+            />
           </div>
-          <div className='border border-gray-400 rounded h-[35px] w-[50%] flex items-center'>
-              <input type="radio" id="female" name="gender" value="female" className="ml-3" />
-                <label htmlFor="female" className='font-outfit p-3 text-[17px] text-gray-500'>Feminino</label>
+
+          <h2 className="font-outfit text-[20px] mb-2 mt-4 block cursor-default">Senha</h2>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Senha"
+            required
+            className="appearance-none font-outfit border border-gray-400 rounded w-full h-[54px] py-5 px-4 text-gray-700 focus:outline-none"
+          />
+
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="bg-blue-600 hover:bg-blue-500 text-white text-lg font-bold font-outfit py-3 px-14 mt-7 rounded focus:outline-none focus:shadow-outline"
+            >
+              Criar conta
+            </button>
           </div>
-        </fieldset>  
-        <h2 className='font-outfit text-[20px] mb-2 mt-4 block cursor-default'>Senha</h2> 
-        <input type="password" name="password" placeholder="Senha" required className="appearance-none font-outfit border border-gray-400 rounded w-full h-[54px] py-5 px-4 text-gray-700 focus:outline-none" />
-        <div className="flex justify-center">
-          <button type="submit" className="bg-blue-600 hover:bg-blue-500 text-white text-lg font-bold font-outfit py-3 px-14 mt-7 rounded focus:outline-none focus:shadow-outline">Criar conta</button>
-        </div>
+        </form>
       </div>
-      
     </section>
   );
 };
 
-export default SingUpPageForm
-
+export default SingUpPageForm;
